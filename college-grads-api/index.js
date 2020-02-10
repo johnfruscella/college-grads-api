de5afa3c32c2fd4491f8bb5a087329cd4a963895
johnfruscella <johnfruscella@gmail.com>
2020-02-09 23:38:41 -0500

    revised route handlers in admin

--- college-grads-api/index.js
+++ college-grads-api/index.js
@@ -6,15 +6,15 @@ require('dotenv/config');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-let databaseConnected = false;
+//let databaseConnected = false;*************************************************
 const homeRoute = require('./routes/home');
 
-app.use('/', data_base_condition, homeRoute)
+app.use('/', homeRoute)
 
 
 const adminRoute = require('./routes/admin');
 
-app.use('/admin', data_base_condition, adminRoute)
+app.use('/admin', adminRoute)
 
 const options = {
     useNewUrlParser: true,
@@ -24,14 +24,8 @@ mongoose.connect(process.env.DB_CONNECTION, options);
 const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Connected to database...'))
-//LOCAL PORT THAT SERVER IS HOSTED ON
-const port = process.env.Port||3000;
+
+const port = process.env.Port||5000;
 app.listen(port, () => console.log(`listening on ${port}...`));
 
-function data_base_condition(req, res, next) {
-    if (databaseConnected === true) {
-        next()
-    } else {
-        res.status(500).json({message: "Server not connected to database"})
-    }
-}
+//process.env.DB_CONNECTION
--- college-grads-api/routes/admin.js
+++ college-grads-api/routes/admin.js
@@ -1,41 +1,25 @@
-//packages used for this project
-const express = require('express'),
-      router = express.Router(),
-      mongoose = require('mongoose');
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
 
-      //this is how this file will interact with the database
-      const StudentSchema = require('../models/Student');
+      
+const StudentSchema = require('../models/Student');
 
 
-    //sets up the admin page as options, served as json
-    let firstTimeRan = true, 
-        firstMessage = {
-            message: "You have been granted access to the administrator route",
-            hint: "Refresh the page to see options as an administrator"
-        },
-        optionsMessage = {
-            message: "You have options to change what is in the database.",
+
+const optionsMessage = {
+            message: "Use the following to add, update, or view graduate information.",
             POST: "route to use: admin/post",
             GET: "route to use: admin/getall or admin/getid/:id",
             DELETE: "route to use: admin/delete/:id",
             PATCH: "route to use: admin/patch/id"
         }
 
-        //the /admin route will be for showing the options of what the admin can do
-        router.get('/', (req, res) => {
+        
+    router.get('/', (req, res) => {
 
-        if (firstTimeRan) {
-
-            res.status(200).json(firstMessage);
-
-            firstTimeRan = false
-            
-        } else {
-            
-            res.status(200).json(optionsMessage);
-
-        }
-            
+        res.status(200).json(optionsMessage);
+        
       })
 
       //POST REQUEST
