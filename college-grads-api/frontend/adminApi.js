fa8c65db4e85bcccbd3e64b5f6b869b5c3421040
johnfruscella <johnfruscella@gmail.com>
2020-02-18 17:03:17 -0500

    added code to adminApi to connect to frontend

--- college-grads-api/frontend/adminApi.js
+++ college-grads-api/frontend/adminApi.js
@@ -1,26 +1,65 @@
 // Post
 document.getElementById('submitPost').addEventListener('click', postRequest);
 
+document.getElementById('submitUpdate').addEventListener('click', putRequest);
+
+document.getElementById('submitDelete').addEventListener('click', deleteRequest);
 
 
 
-async function postdata() {
 
-    let postJson = (create_Obj('postStudent'));
+async function postRequest() {
 
-     document.getElementById('request_message').innerText = 'Summiting';
 
-     await fetch('http://localhost:3000/admin', {
-         method: 'POST',
-         headers: {
-           'Accept': 'application/json',
-           'Content-Type': 'application/json'
-         },
+  let postJson = (create_Obj('postStudent'));
 
-         body: postJson
+
+   document.getElementById('requestMessage').innerText = 'Summiting';
+
+   await fetch('http://localhost:3000/admin', {
+       method: 'POST',
+       headers: {
+         'Accept': 'application/json',
+         'Content-Type': 'application/json'
+       },
+
+       body: postJson
+
+   })
+
+     //returns the response from the api and parses from readableStream to JSON
+     .then( readable_stream_res => {
+
+         return readable_stream_res.json()
+     })
+
+     //the json response is used to display status code/errors to the client
+     .then( parsedResponse => {
+
+         if (!checkStatusOk(parsedResponse)) {
+             return 
+         }
+
+       //   console.log(parsedResponse.document);
+
+         let newPostDocument = parsedResponse.document,
+
+         PostHTML = display_doc_info(newPostDocument);
+         
+         document.getElementById('responseElm').innerHTML = `New Post:${PostHTML}`;
+
+         document.getElementById('requestMessage').innerText = 'Student Successfully Created';
+         
 
      })
-    .then(res => { return res.json() })
-    .then(data => console.log(data))
-    .catch(error => console.log('John ERROR'))
-    }
+
+     .catch( err => {
+
+         console.log(err);
+         
+     })
+
+     .finally( () => {clear_formData(postStudent); setTimeout(reset_req_mes, 3000)})
+
+
+}
